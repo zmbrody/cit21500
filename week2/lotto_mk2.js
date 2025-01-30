c10dc58b8e90b6d298ed0d02a7a096452ca56d36
zmbrody <zacharybrody98@yahoo.com>
2025-01-30 16:12:55 -0500

    lotto functions assigment

---
+++ week2/lotto_mk2.js
@@ -0,0 +1,19 @@
+function lotteryNumber() 
+{
+    /* 
+    Returns a number between 1 and 49, inclusive
+    */
+    let FoundNum = (1 + Math.floor(Math.random() * 49));
+    return(FoundNum);
+}
+
+const getSixNumbers = function ()
+{
+    let TheNumbers = "";
+    for (let i = 0; i < 6; i++)
+    {
+        let RandNum = Math.floor(Math.random() * 50);
+        console.log(RandNum);
+    }
+    return;
+}
