0adf0dd7d65eed41d24aa9388bbd168c8ddbdc64
zmbrody <zacharybrody98@yahoo.com>
2025-02-12 14:43:19 -0500

    I, Object assignment

---
+++ week5/i_object.js
@@ -0,0 +1,42 @@
+const robot =
+{
+    name : "Robo",
+    manufacturer : "Manufacturer Inc.",
+    skillset : 
+    {
+        skill1 : "eating",
+        skill2 : "sleeping",
+        skill3 : "cooking",
+    },
+    hasSkill : function(skill)
+    {
+        if (this.skillset.skill1 == skill || this.skillset.skill2 == skill || this.skillset.skill3 == skill)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    },
+}
+
+function main()
+{
+    console.log(`The robot is called ${robot.name}. It was manufactured by ${robot.manufacturer}. ` + 
+        `It is capable of ${robot.skillset.skill1}, ${robot.skillset.skill2}, and ${robot.skillset.skill3}.`);
+    
+    let UsrSkill = prompt("What skill do you want to check?")
+    UsrSkill = UsrSkill.toLowerCase();
+    
+    if (robot.hasSkill(UsrSkill))
+    {
+        console.log(`Yup, the robot can do ${UsrSkill}.`)
+    }
+    else
+    {
+        console.log(`Sorry, the robot can't do ${UsrSkill}.`)
+    }
+
+}
+main();
