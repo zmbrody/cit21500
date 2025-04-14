c34de615d5c48e02880be066ae750353fd49bd05
zmbrody <zacharybrody98@yahoo.com>
2025-04-14 10:45:58 -0400

    Update cocktails assigment

--- week11/cocktails.js
+++ week11/cocktails.js
@@ -6,10 +6,10 @@ const SpawnCocktails = function()
 	let output_html = "";
 	cocktails.forEach((cocktail)=>
 	{
-		output_html = `<div class="box">`
-					+ `<img class="cocktail_pic" src="images/cocktails/${cocktail.cocktail_id}.jpg" alt="${cocktail.cocktail_name}"/>`
-					+ `<p class="cocktail_name"></p>`
-					+ `<p class="cocktail_info"></p>`
+		output_html = `<table class="box">`
+					+ `<tr><td><p class=cocktail_pic_wrapper><img class="cocktail_pic" src="images/cocktails/${cocktail.cocktail_id}.webp" alt="${cocktail.cocktail_name}"/></p></td></tr>`
+					+ `<tr><td><p class="cocktail_name"></p></td></tr>`
+					+ `<tr><td><p class="cocktail_info"></p></td></tr>`
 					+ `</div>`;
 
 		html_wrapper.append(output_html);
@@ -25,7 +25,7 @@ const Init = function()
 	jQuery.each($(".box"), function(i)
 	{
 		$(".cocktail_name").eq(i).text(cocktails[i]["cocktail_name"]);
-		$(".cocktail_info").eq(i).text(`Ingredients: ${cocktails[i]["ingredients"]}`);
+		$(".cocktail_info").eq(i).html(`Ingredients: ${cocktails[i]["ingredients"]}`);
 	});
 
 	// define which functions to call when events are fired
@@ -33,7 +33,7 @@ const Init = function()
 	{
 		// OnClick
 		let i = $(".box").index(this);
-		let output_html = `Ingredients: ${cocktails[i]["ingredients"]}<br>Calories:${cocktails[i]["calories"]}<br><br>${cocktails[i]["recipe"]}`;
+		let output_html = `Calories: ${cocktails[i]["calories"]}<br><br>Ingredients: ${cocktails[i]["ingredients"]}<br><br>${cocktails[i]["recipe"]}`;
 		$(this).find(".cocktail_info").html(output_html);
 	});
 	
@@ -48,7 +48,7 @@ const Init = function()
 	{
 		// OnStopTouch
 		let i = $(".box").index(this);
-		$(this).find(".cocktail_info").text(`Ingredients: ${cocktails[i]["ingredients"]}`);
+		$(this).find(".cocktail_info").html(`Ingredients: ${cocktails[i]["ingredients"]}`);
 	});
 }
 
